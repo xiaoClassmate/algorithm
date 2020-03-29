da0a427a4d61412c351fb71cb6c1c802e07b3bb7
xiaoClassamte <s1410643030@gms.nutc.edu.tw>
2020-03-30 01:06:38 +0800

    add powerset and itSum

---
+++ sortOrder.py
@@ -0,0 +1,46 @@
+import pretty_errors
+
+# def mergeSort(priceList):
+# 	if len(priceList) > 1:
+# 		middle = len(priceList) // 2
+# 		left = priceList[:middle]
+# 		right = priceList[middle:]
+
+# 		mergeSort(left)
+# 		mergeSort(right)
+
+# 		L = 0 # iterator for traversing the left
+# 		R = 0 # iterator for traversing the right
+# 		K = 0 # Iterator for the main list
+
+# 		# sort with decreasing order
+# 		while L < len(left) and R < len(right):
+# 			if left[L] > right[R]:
+# 				priceList[K] = left[L]
+# 				L += 1
+# 			else:
+# 				priceList[K] = right[R]
+# 				R += 1
+# 			K += 1
+
+# 		# for all the remaining values
+# 		while L < len(left):
+# 			priceList[K] = left[L]
+# 			L += 1
+# 			K += 1
+
+# 		while R < len(right):
+# 			priceList[K] = right[R]
+# 			R += 1
+# 			K += 1
+
+priceList = [20, 33, 50, 18]
+priceList.sort()
+priceList.reverse()
+# mergeSort(priceList)
+
+
+
+
+
+
