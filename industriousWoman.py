978b78395898e81a0dc6aacfc9e0b7fc936ef983
xiaoClassamte <s1410643030@gms.nutc.edu.tw>
2020-04-17 00:42:21 +0800

    target_sum_list

--- industriousWoman.py
+++ industriousWoman.py
@@ -27,43 +27,15 @@ def backpack(json, target_sum, index = 0):
                 return path + [{ 'serial': item['serial'], 'value': item['value'], 'number': number, 'name': item['name']}]
             return path
 
-target_sum = int(input('Please enter the Price you want to split : '))
-result = backpack(json, target_sum)
-print(result)
+target_sum = str(input('Please enter the Price you want to split : '))
+target_sum_list = []
+for i in range(2):
+    target_sum_list.append(int(target_sum.split(' ')[i]))
+print(target_sum_list)
+# result = backpack(json, target_sum)
+# print(result)
 
-# ------------------------- 測試結果 ---------------------------------
 
-# name = 商品名稱
-# serial = 商品編號
-# value = 商品價格
-# number = 商品數量
-
-# Please enter the Price you want to split : 102
-# [{'name': 'TRAINING SOCKS MID 3-PACK', 'serial': 99, 'number': 1, 'value': 12},
-# {'name': "Men's Breath Thermo® Half Zip", 'serial': 76, 'number': 1, 'value': 90}]
-
-# 90*1 + 12*1 = 102
-
-# Please enter the Price you want to split : 79
-# [{'number': 1, 'serial': 49, 'name': 'WARMALITE PIP', 'value': 24},
-# {'number': 1, 'serial': 41, 'name': "MEN'S AERO 4.5 SHORT 2.0", 'value': 55}]
-
-# 55*1 + 21*1 = 79
-
-# Please enter the Price you want to split : 327
-# [{'number': 1, 'value': 12, 'serial': 99, 'name': 'TRAINING SOCKS MID 3-PACK'},
-# {'number': 1, 'value': 45, 'serial': 53, 'name': "Men's Alpha 9 Running Shorts"},
-# {'number': 3, 'value': 90, 'serial': 76, 'name': "Men's Breath Thermo® Half Zip"}]
-
-# 90*3 + 45*1 + 12*1 = 327
-
-# Please enter the Price you want to split : 666
-# [{'value': 24, 'number': 1, 'serial': 49, 'name': 'WARMALITE PIP'},
-# {'value': 32, 'number': 1, 'serial': 7, 'name': "Women's Patriotic Tank"},
-# {'value': 85, 'number': 4, 'serial': 52, 'name': "Men's Breath Thermo® Base Layer Long Sleeve"},
-# {'value': 90, 'number': 3, 'serial': 76, 'name': "Men's Breath Thermo® Half Zip"}]
-
-# 90*3 + 85*4 + 32*1 + 24*1 = 666
 
 
 			
