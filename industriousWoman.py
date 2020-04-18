84007ee8480769ed643d3f581dd9339aaa2dae57
xiaoClassamte <s1410643030@gms.nutc.edu.tw>
2020-04-18 23:30:49 +0800

    ps

--- industriousWoman.py
+++ industriousWoman.py
@@ -1,23 +1,29 @@
 import pretty_errors
 import json
 
-# 導入商品清單(JSON檔)
+# 導入商品清單(JSON)
 with open("/home/xiao/gitReadWrite/algorithm/goodsMenu/goodsMenu/json/goodsMenu.json") as f:
     goodsMenu = json.load(f)
 
 # 必買物功能
 def must_buy(target_sum):
+
+    # 必買物序號
     must_buy = str(input('Please enter the serial you must to buy : '))         
     must_buy_list = []
-    must_buy_path = []
+    
     must_buy_length = len(must_buy.split(' '))
     for i in range(0, must_buy_length):
+        # 將序號存到 list 中
         must_buy_list.append(int(must_buy.split(' ')[i]))
+        # 根據序號在 JSON 檔中取出與 serial 相對應的 value 值
         item = goodsMenu[must_buy_list[i]]
-        # print(item['value'])
+        # 實際要拆分的錢 = 拆分的錢 - 必買物價格
         target_sum -= item['value']
         real_target_sum = target_sum
+        # 印出買了哪些 "必買物"
         print([{'serial': item['serial'], 'value': item['value'], 'number':1}])
+    # 回傳實際要拆分的錢
     return real_target_sum
 
 
@@ -58,33 +64,46 @@ print(real_target_sum)
 print(result)
 
 # 拆兩筆
-# target_sum = str(input('Please enter the Price you want to split : '))
-# target_sum_list = []
-# target_sum_length = len(target_sum.split(' '))
-# for i in range(target_sum_length):
-#     try:
-#         target_sum_list.append(int(target_sum.split(' ')[i]))
-#         target_sum_list = sorted(target_sum_list, reverse=False)
-#         result = backpack(json, target_sum_list[i])
-#         print(result)
-#         # print(target_sum_list)
-#     except:
-#         pass
+target_sum = str(input('Please enter the Price you want to split : '))
+target_sum_list = []
+target_sum_length = len(target_sum.split(' '))
+for i in range(target_sum_length):
+    try:
+        target_sum_list.append(int(target_sum.split(' ')[i]))
+        target_sum_list = sorted(target_sum_list, reverse=False)
+        result = backpack(json, target_sum_list[i])
+        print(result)
+        # print(target_sum_list)
+    except:
+        pass
 
-# ------- 測試 -------
+# ------- 更新 -------
+# 4/18 更新：單一必買物完成
+# 已完成：單一必買物、拆一筆、拆兩筆
+# 待完成：多數必買物、拆兩筆的總體最佳問題
+
+# ------- 拆一筆 + 必買物測試 -------
+# Please enter the Price you want to split : 127 (要拆分的錢)
+# Please enter the serial you must to buy : 0 1 (必買物的序號)
+# [{'value': 14, 'serial': 0, 'number': 1}]
+# [{'value': 13, 'serial': 1, 'number': 1}]
+# 100 (扣掉必買物後真實要拆分的錢)
+# [{'value': 10, 'serial': 4, 'number': 1}, {'value': 90, 'serial': 76, 'number': 1}]
+
+# Please enter the Price you want to split : 123 (要拆分的錢)
+# Please enter the serial you must to buy : 1 2 3 (必買物的序號)
+# [{'number': 1, 'value': 13, 'serial': 1}]
+# [{'number': 1, 'value': 25, 'serial': 2}]
+# [{'number': 1, 'value': 30, 'serial': 3}]
+# 55 (扣掉必買物後真實要拆分的錢)
+# [{'number': 1, 'value': 55, 'serial': 41}]
+
+# ------- 拆兩筆測試 -------
 # Please enter the Price you want to split : 123 456
-# [{'value': 13, 'number': 1}, {'value': 20, 'number': 1}, {'value': 90, 'number': 1}]
-# [{'value': 24, 'number': 1}, {'value': 82, 'number': 1}, {'value': 85, 'number': 2}, {'value': 90, 'number': 2}]
-
-# ------- 說明 -------
 # json 檔 'value': 90, 'number': 3
 # 第一筆 123 已經拿過 1 個 90，因此第二筆 456 剩 2 個 90 可拿
-
-# ------- 結果 -------
-# 4/18 更新：單一必買物完成
-# ---------------------
-# 已完成：'可以剛好拆分'成功
-# 待完成：多筆必買物、其中有無法拆分成功的情況、總體最佳
+# [{'value': 13, 'number': 1}, {'value': 20, 'number': 1}, {'value': 90, 'number': 1}]
+# [{'value': 24, 'number': 1}, {'value': 82, 'number': 1}, {'value': 85, 'number': 2}, {'value': 90, 'number': 2}]
 
 
 			
