c245a72e36797316c91b08a55bab9a8b92166ed4
xiaoClassamte <s1410643030@gms.nutc.edu.tw>
2020-04-19 19:34:00 +0800

    must_buy_debug

--- industriousWoman.py
+++ industriousWoman.py
@@ -1,5 +1,7 @@
 import pretty_errors
 import json
+import sys
+sys.setrecursionlimit(1000000)
 
 # 導入商品清單(JSON)
 with open("/home/xiao/gitReadWrite/algorithm/goodsMenu/goodsMenu/json/goodsMenu.json") as f:
@@ -7,60 +9,69 @@ with open("/home/xiao/gitReadWrite/algorithm/goodsMenu/goodsMenu/json/goodsMenu.
 
 # 必買物功能
 def must_buy(target_sum):
-    # 輸入必買物
-    must_buy = str(input('Please enter the serial and number that you must to buy : '))         
+    # 輸入必買物，格式：[編號A 數量 編號B 數量 編號C 數量...]
+    must_buy = str(input('Please enter the serial and number that you must to buy : '))
+
     # 必買物的產品編號
     must_buy_serial = []
+
     # 必買的數量
     must_buy_number = []
-    must_buy_length = len(must_buy.split(' '))
-    print(must_buy_length)
 
+    # 將序號跟數量分別存到各自的 list 中 
+    must_buy_length = len(must_buy.split(' '))
     for i in range(0, must_buy_length):
-        # 將序號跟數量分別存到各自的 list 中
         if(i % 2 == 0):
             must_buy_serial.append(int(must_buy.split(' ')[i]))
         else:
             must_buy_number.append(int(must_buy.split(' ')[i]))
 
-    # DEBUG 用
+    # DEBUG
     # print(must_buy_serial)
     # print(must_buy_number)
 
     for i in range(0, must_buy_length // 2):
-        # 根據序號在 JSON 檔中取出與 serial 相對應的 value 值
+        # JSON 索引設定
         item = goodsMenu[must_buy_serial[i]]
 
         # 判斷數量是否合乎邏輯
         if item['number'] < must_buy_number[i]:
-            print('產品編號 '+ str(item['serial']) +' ，必買數量超過可接受數量')
+            return ('產品編號 '+ str(item['serial']) +' ，必買數量超過可接受數量')
 
         # 實際要拆分的錢 = 拆分的錢 - 必買物價格*數量
         target_sum -= item['value'] * must_buy_number[i]
         real_target_sum = target_sum
 
         # 印出買了哪些 "必買物"
-        print([{'serial': item['serial'], 'value': item['value'], 'number':must_buy_number[i]}])
+        print ([{'serial': item['serial'], 'value': item['value'], 'number':must_buy_number[i]}])
+
+        # 紀錄已經拿了幾個，並扣除，避免拿取已經拿過的產品
+        item['number'] -= must_buy_number[i]
 
     # 回傳實際要拆分的錢
+    print('實際要拆分 = ' + str(real_target_sum) + ' 元')
     return real_target_sum
 
 # 主要拆分演算法
-def backpack(goodsMenu, real_target_sum, target_sum, index = 0):
+def backpack(goodsMenu, real_target_sum, index = 0):
+    # print('參數 real_target_sum 收到的值 = ' + str(real_target_sum))
+
     # 商品由大到小排序
     goodsMenu = sorted(goodsMenu , key = lambda i: i['value'], reverse=True)
 
-    # 必買物已經超過門檻
-    if real_target_sum <= 0:
-        # 回傳必買物總金額
-        return target_sum - real_target_sum
+    # 必買物已經超過門檻，回傳必買物總金額
+    if int(real_target_sum) <= 0:
+        return []
 
+    # 沒有辦法剛好拆分
     if index >= len(goodsMenu):
         return None
 
     # JSON 索引設定
     item = goodsMenu[index]
     index += 1
+
+    # 可以拿的數量 = min()
     canTake = min(real_target_sum // item['value'], item['number'])
 
     # 遞迴
@@ -68,17 +79,15 @@ def backpack(goodsMenu, real_target_sum, target_sum, index = 0):
         path = backpack(goodsMenu, real_target_sum - item['value'] * number, index)
         if path != None: 
             if number:
+                # 紀錄已經拿了幾個，並扣除，避免拿取已經拿過的產品
                 item['number'] -= number
-                # print({'name': item['name'], 'value': item['value'], 'number': item['number']})
-                return path + [{'serial': item['serial'], 'value': item['value'], 'number': number}]
-            # 'name': item['name'],
+                return path + [{'serial': item['serial'], 'value': item['value'], 'number': number}]  # 'name': item['name'],
             return path
 
 # 拆一筆
 target_sum = int(input('Please enter the Price you want to split : '))
 real_target_sum = must_buy(target_sum)
-result = backpack(goodsMenu, real_target_sum, target_sum)
-print(real_target_sum)
+result = backpack(goodsMenu, real_target_sum)
 print(result)
 
 # 拆兩筆
