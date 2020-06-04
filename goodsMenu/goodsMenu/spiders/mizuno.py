6cc77c36a97ff98e08153071171010cf87ca0e78
xiaoClassmate <s1410643030@gms.nutc.edu.tw>
2020-06-04 14:32:51 +0800

    json scrapy 更新

--- goodsMenu/goodsMenu/spiders/mizuno.py
+++ goodsMenu/goodsMenu/spiders/mizuno.py
@@ -22,9 +22,7 @@ class MizunoSpider(CrawlSpider):
 	serial = -1
 
 	def start_requests(self):
-		urls = ['https://www.mizunousa.com/category/sports/running/sports+running+apparel.do?c=100262.100269.100356&sortby=bestSellersAscend&pp=100',
-				'https://www.mizunousa.com/category/sports/golf/accessories.do?c=100262.100351.100363&sortby=ourPicksAscend&pp=100',
-				'https://www.mizunousa.com/category/sports/running/accessories.do?c=100262.100269.100357&sortby=ourPicksAscend&page=all']
+		urls = 'https://www.mizunousa.com/category/sports/running/sports+running+apparel.do?c=100262.100269.100356&sortby=bestSellersAscend&pp=100'
 		yield scrapy.Request(urls, self.parst_list)
 
 	def parst_list(self, response):
--- goodsMenu/scrapy_run.py
+++ goodsMenu/scrapy_run.py
@@ -3,7 +3,7 @@ import time
 
 brands = ['mizuno']
 for b in brands:
-	cmd = 'scrapy crawl ' + b + ' -o /home/xiao/gitReadWrite/algorithm/goodsMenu/goodsMenu/json/goodsMenu.json'
+	cmd = 'scrapy crawl ' + b + ' -o goodsMenu.json'
 	print(cmd)
 	os.system(cmd)
 
