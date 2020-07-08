c5aa31132d1343c4583c89625ce6be66f7ac19cb
xiaoClassmate <s1410643030@gms.nutc.edu.tw>
2020-07-08 20:15:05 +0800

    flask

--- flask/app.py
+++ flask/app.py
@@ -1,28 +1,40 @@
-from flask import Flask, url_for, redirect, render_template
+from flask import Flask, url_for, redirect, render_template, request
 
 app = Flask(__name__)
 
+# 渲染模板
 @app.route('/')
 def index():
-    return 'hello world'
+    return render_template('index.html')
 
+# 渲染模板(帶 str 參數)
 @app.route('/user/<username>')
 def username(username):
-    return 'I am ' + username
+    return render_template('index.html', templates_user = username)
 
+# 帶非 str 參數
 @app.route('/age/<float:age>')
 def age(age):
     return 'I am ' + str(age) + ' years old.'
 
+# url_for 應用
 @app.route('/a')
 def a():
 	return 'here is a'
-
 @app.route('/b')
 def b():
 	return redirect(url_for('a'))
 
+# GET 與 POST
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		return 'Hello ' + request.values['username']
+	else:
+		return render_template('login.html')
+
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run()    
+
 
