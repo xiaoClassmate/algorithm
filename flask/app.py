d2449939e682c2d14caf81bb74ef35c8835c75fc
xiaoClassmate <s1410643030@gms.nutc.edu.tw>
2020-07-15 22:42:35 +0800

    update

--- flask/app.py
+++ flask/app.py
@@ -9,7 +9,7 @@ def index():
 
 # 渲染模板(帶 str 參數)
 @app.route('/user/<username>')
-def username(username):
+def user(username):
     return render_template('index.html', templates_user = username)
 
 # 帶非 str 參數
@@ -26,12 +26,21 @@ def b():
 	return redirect(url_for('a'))
 
 # GET 與 POST
-@app.route('/login', methods = ['GET', 'POST'])
+# @app.route('/login', methods=['GET', 'POST']) 
+# def login():
+#     if request.method == 'POST': 
+#         return 'Hello ' + request.values['username'] 
+
+#     return "<form method='post' action='/login'><input type='text' name='username' />" \
+#             "</br>" \
+#            "<button type='submit'>Submit</button></form>"
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	if request.method == 'POST':
-		return redirect(url_for('index', templates_user = request.form.get('username')))
-	else:
-		return render_template('login.html')
+    if request.method == 'POST':
+        return redirect(url_for('user', username = request.form.get('username')))
+    else:
+    	return render_template('login.html')
 
 if __name__ == '__main__':
     app.debug = True
--- flask/firebaseTest.py
+++ flask/firebaseTest.py
@@ -3,21 +3,21 @@ import json
 
 firebase = firebase.FirebaseApplication('https://industriouswoman-myg36t91.firebaseio.com/', None)
 data = {
-	'serial' : 2,
-	'name' : 'thrids',
+	'serial' : 10,
+	'name' : 'aaaaaaaaa',
 	'price' : 12,
 	'number' : 7
 }
 # # post
 # result = firebase.post('/goods', data)
 
-# update
-# result = firebase.put('/goods/-MCFwDC4iPnaBlnMiBEc', 'name', 'oooo')
+# # update
+# result = firebase.put('/goods/-MCHi-uxVTVPd7-zvD0X', 'name', 'bbbbbbbb')
 
-# # get
+# get
 # result = firebase.get('/goods', '')
 
-# # delete
-# result = firebase.delete('/goods', 'key')
+# delete
+result = firebase.delete('/goods', '-MCHi-uxVTVPd7-zvD0X')
 
-print(result)
+# print(result)
