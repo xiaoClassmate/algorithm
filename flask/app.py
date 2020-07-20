73164cf406dfb142c8f3a555fb5033b6075dec97
xiaoClassmate <s1410643030@gms.nutc.edu.tw>
2020-07-21 04:48:03 +0800

    create

--- flask/app.py
+++ flask/app.py
@@ -14,7 +14,7 @@ import os
 
 app = Flask(__name__)
 
-config = {
+firebaseConfig = {
   "apiKey": os.getenv("apiKey"),
   "authDomain": os.getenv("authDomain"),
   "databaseURL": os.getenv("databaseURL"),
@@ -22,48 +22,42 @@ config = {
   "serviceAccount": os.getenv("serviceAccount")
 }
 
-firebase = pyrebase.initialize_app(config)
-print(config)
+# 渲染模板
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# # 渲染模板
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+# 渲染模板(帶 str 參數)
+@app.route('/user/<username>')
+def user(username):
+    return render_template('index.html', templates_user = username)
 
-# # 渲染模板(帶 str 參數)
-# @app.route('/user/<username>')
-# def user(username):
-#     return render_template('index.html', templates_user = username)
+# 帶非 str 參數
+@app.route('/age/<float:age>')
+def age(age):
+    return 'I am ' + str(age) + ' years old.'
 
-# # 帶非 str 參數
-# @app.route('/age/<float:age>')
-# def age(age):
-#     return 'I am ' + str(age) + ' years old.'
+# url_for 應用
+@app.route('/a')
+def a():
+	return 'here is a'
+@app.route('/b')
+def b():
+	return redirect(url_for('a'))
 
-# # url_for 應用
-# @app.route('/a')
-# def a():
-# 	return 'here is a'
-# @app.route('/b')
-# def b():
-# 	return redirect(url_for('a'))
-
-# # GET 與 POST
-# # @app.route('/login', methods=['GET', 'POST']) 
-# # def login():
-# #     if request.method == 'POST': 
-# #         return 'Hello ' + request.values['username'] 
-
-# #     return "<form method='post' action='/login'><input type='text' name='username' />" \
-# #             "</br>" \
-# #            "<button type='submit'>Submit</button></form>"
-
-# @app.route('/login', methods=['GET', 'POST'])
+# GET 與 POST
+# @app.route('/login', methods=['GET', 'POST']) 
 # def login():
-#     if request.method == 'POST':
-#         return redirect(url_for('user', username = request.form.get('username')))
-#     else:
-#     	return render_template('login.html')
+#     if request.method == 'POST': 
+#         return 'Hello ' + request.values['username'] 
+
+#     return "<form method='post' action='/login'><input type='text' name='username' />" \
+#             "</br>" \
+#            "<button type='submit'>Submit</button></form>"
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 if __name__ == '__main__':
     app.debug = True
