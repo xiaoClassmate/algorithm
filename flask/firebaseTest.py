2cbcf6c568feb1b1279b102f1dd63d8c22b46901
xiaoClassmate <s1410643030@gms.nutc.edu.tw>
2020-07-25 17:57:41 +0800

    CR

--- flask/firebaseTest.py
+++ flask/firebaseTest.py
@@ -1,23 +1,59 @@
-from firebase import firebase
-import json
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- bootstrap.css -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    
+    <meta charset="UTF-8">
+    <title>Hello</title>
+</head>
+<body>
+    <!-- firebase.js -->
+    <script src="https://www.gstatic.com/firebasejs/7.2.0/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.2.0/firebase-database.js"></script>
 
-firebase = firebase.FirebaseApplication('https://industriouswoman-myg36t91.firebaseio.com/', None)
-data = {
-	'serial' : 10,
-	'name' : 'aaaaaaaaa',
-	'price' : 12,
-	'number' : 7
-}
-# # post
-# result = firebase.post('/goods', data)
+    <!-- firebase-config -->
+    <script>
+        var firebaseConfig = {
+            apiKey: "AIzaSyCDuyQEcT7FbDI8kJkQ4TzP0Un3F4BwC3A",
+            authDomain: "industriouswoman-myg36t91.firebaseapp.com",
+            databaseURL: "https://industriouswoman-myg36t91.firebaseio.com",
+            projectId: "industriouswoman-myg36t91",
+            storageBucket: "industriouswoman-myg36t91.appspot.com",
+            messagingSenderId: "902295424134",
+            appId: "1:902295424134:web:7876cc39ae5fd3a0ea9507",
+            measurementId: "G-DXZ0LF42BC"
+        };
 
-# # update
-# result = firebase.put('/goods/-MCHi-uxVTVPd7-zvD0X', 'name', 'bbbbbbbb')
+        firebase.initializeApp(firebaseConfig);
 
-# get
-# result = firebase.get('/goods', '')
+        function writeData(){
+            firebase.database().ref("goods/").push({
+                name: document.getElementById("goodsName").value,
+                price: document.getElementById("goodsPrice").value,
+                number: document.getElementById("goodsNumber").value
+            });
+        }
+    </script>
 
-# delete
-result = firebase.delete('/goods', '-MCHi-uxVTVPd7-zvD0X')
+    <!-- add goods form -->
+    <div class="container" style="margin-top: 10%">
+        <div class="row">
+            <form id="form">
+                <div class="form-group">
+                    name:<input type="text" class="form-control" id="goodsName">
+                    price:<input type="text" class="form-control" id="goodsPrice">
+                    number:<input type="text" class="form-control" id="goodsNumber">
+                </div>
+                    <button id="createGoods" onclick="writeData()" class="btn btn-success">新增物品</button>
+            </form>
+        </div>
+    </div>
 
-# print(result)
+    <!-- bootstrap.js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
+
